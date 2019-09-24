b18f7f262b349470c716c033d79cd69bf1b269e2
SpecialTheK <Christian.Heideman@gmail.com>
2019-09-24 10:08:21 +0300

    Initial commit

---
+++ app/src/main/java/com/speciial/travelchest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.speciial.travelchest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
