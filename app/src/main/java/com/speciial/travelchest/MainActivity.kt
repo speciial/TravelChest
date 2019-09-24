1a8f2e3718cf24afabb991a209d307547cb6945d
SpecialTheK <Christian.Heideman@gmail.com>
2019-09-24 16:21:38 +0300

    Added dependencies for ARCore and implemented basic billboard placement

--- app/src/main/java/com/speciial/travelchest/MainActivity.kt
+++ app/src/main/java/com/speciial/travelchest/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.speciial.travelchest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.ux.ArFragment
+import com.speciial.travelchest.arview.ARBillboard
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+
+    private lateinit var billboard: ARBillboard
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            Log.d(TAG, "Plane tapped!")
+
+            val anchorNode = AnchorNode(hitResult.createAnchor())
+            anchorNode.setParent(arFragment.arSceneView.scene)
+
+            billboard = ARBillboard(this, arFragment.transformationSystem, anchorNode)
+        }
     }
+
+    companion object {
+        const val TAG = "TRAVEL_CHEST"
+    }
+
 }
---
+++ app/src/main/java/com/speciial/travelchest/arview/ARBillboard.kt
@@ -0,0 +1,32 @@
+package com.speciial.travelchest.arview
+
+import android.content.Context
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.TransformableNode
+import com.google.ar.sceneform.ux.TransformationSystem
+import com.speciial.travelchest.R
+
+class ARBillboard(
+    context: Context,
+    transformationSystem: TransformationSystem,
+    anchorNode: AnchorNode
+) {
+
+    private var placementNode: TransformableNode = TransformableNode(transformationSystem)
+
+    private lateinit var renderable: ViewRenderable
+
+    init {
+        ViewRenderable.builder()
+            .setView(context, R.layout.default_billboard_view)
+            .build()
+            .thenAccept{
+                renderable = it
+
+                placementNode.setParent(anchorNode)
+                placementNode.renderable = renderable
+            }
+    }
+
+}
